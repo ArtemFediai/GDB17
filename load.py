2fc7b6ff4c8ada6e86c0a22d774bd5fe327fed98
artem <artem.fediai@kit.edu>
2021-11-10 11:42:49 +0100

    load / main upd: permuted numbers

--- load.py
+++ load.py
@@ -9,9 +9,7 @@ import pickle
 DATA_FOLDER = '/home/artem/dataset_gdb17'
 DATA_FILE_NAME = 'GDB17.50000000.smi'
 DATA_PATH = DATA_FOLDER + '/' + DATA_FILE_NAME
-MOL_NUM_LIMIT = 1_000_000
 # MOL_NUM_LIMIT = False  # if False, the whole data set
-
 RDLogger.DisableLog('rdApp.*')
 
 
@@ -21,24 +19,28 @@ def main():
     atoms_count_list = []
     unique_atom_counts = []
     occurences_number = []
+    permuted_numbers = []
 
     save_dict = {
         'atom_count_list.pkl': atoms_count_list,
         'unique_atom_counts.pkl': unique_atom_counts,
-        'occurences_number.pkl': occurences_number
+        'occurences_number.pkl': occurences_number,
+        'permuted_numbers.pkl': permuted_numbers
     }
 
     for save_keys in save_dict:
         with open(DATA_FOLDER + '/' + save_keys, 'rb') as fid:
             save_dict[save_keys] = pickle.load(fid)
 
-    atoms_count_list = save_dict['atom_count_list.pkl']
-    unique_atom_counts = save_dict['unique_atom_counts.pkl']
+    atoms_count_list = save_dict['atom_count_list.pkl'] #
+    unique_atom_counts = save_dict['unique_atom_counts.pkl']  # number of atoms
     occurences_number = save_dict['occurences_number.pkl']
-
+    permuted_numbers = save_dict['permuted_numbers.pkl']
 
     ax = seaborn.countplot(atoms_count_list)
     plt.show()
+
+    print(f'Number of atoms per molecules in the dataset: {unique_atom_counts}')
 
 if __name__ == '__main__':
     main()
--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def main(MOL_NUM_LIMIT=None):
     DATA_PATH = DATA_FOLDER + '/' + DATA_FILE_NAME
     suppl = Chem.SmilesMolSupplier(DATA_PATH)
     data_length = len(suppl)
-    print(f'length of the data set: {Decimal(data_length):.2E}')
+    print(f'length of the whole data set: {Decimal(data_length):.2E}')
 
     if not MOL_NUM_LIMIT:
         MOL_NUM_LIMIT = data_length  # <---!
@@ -31,14 +31,14 @@ def main(MOL_NUM_LIMIT=None):
         MOL_NUM_LIMIT = MOL_NUM_LIMIT
 
     # randomly permuted molecule numeration
-    permuted_numbers_int_64 = np.random.permutation(data_length).astype(int)
+    permuted_numbers_int_64 = np.random.permutation(data_length).astype(int)  # 64bit does not work for rdkit
     permuted_numbers = permuted_numbers_int_64.tolist()
 
-    numbers_of_atoms = [10, 11, 12, 13, 14, 15, 16, 17]
+    # numbers_of_atoms = [10, 11, 12, 13, 14, 15, 16, 17]
 
-    idx_vs_num_atoms = {}
-    for i, num_atoms in enumerate(numbers_of_atoms):
-        idx_vs_num_atoms[num_atoms] = i
+    # idx_vs_num_atoms = {}
+    # for i, num_atoms in enumerate(numbers_of_atoms):
+    #     idx_vs_num_atoms[num_atoms] = i
 
     atoms_count_list = [suppl[i].GetNumAtoms() for i in permuted_numbers[:MOL_NUM_LIMIT]]  # takes 1 h
 
@@ -46,13 +46,23 @@ def main(MOL_NUM_LIMIT=None):
     occurences_number = np.bincount(atoms_count_list)
 
     ax = seaborn.countplot(atoms_count_list)
-    plt.show()
+    fig = ax.get_figure()
 
-    # save everything
+    # save plot num occurences of X-atoms molecule
+    current_path = DATA_FOLDER + '/' + 'atoms_count_list.png'
+    if not os.path.exists(DATA_FOLDER + '/' + 'atoms_count_list.png'):
+        print(f'Saving atoms_count_list into {current_path}...')
+        fig.savefig(current_path)
+        print('...Saved successfully')
+    else:
+        print(f'the path {current_path} exists. Will not save the Figure')
+
+    # save data
     save_dict = {
         'atom_count_list.pkl': atoms_count_list,
         'unique_atom_counts.pkl': unique_atom_counts,
-        'occurences_number.pkl': occurences_number
+        'occurences_number.pkl': occurences_number,
+        'permuted_numbers.pkl': permuted_numbers,
     }
 
     for save_key in save_dict:
