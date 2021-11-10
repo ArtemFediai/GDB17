c56581eb7c1bfc1dc1e10e268038b6b5d467b974
artem <artem.fediai@kit.edu>
2021-11-10 20:38:09 +0100

    save dict with all mol idx with X atoms and into the folder 100 those idx

--- load.py
+++ load.py
@@ -1,17 +1,22 @@
+# load and get 100 mols of every atoms count
+import os.path
 from decimal import Decimal
 from rdkit import Chem
 from rdkit import RDLogger
 import numpy as np
+import random
 import matplotlib.pyplot as plt
 import seaborn
 import pickle
+import os
 RDLogger.DisableLog('rdApp.*')
 
 DATA_FOLDER = '/home/artem/dataset_gdb17'
 DATA_FILE_NAME = 'GDB17.50000000.smi'
 DATA_PATH = DATA_FOLDER + '/' + DATA_FILE_NAME
 # MOL_NUM_LIMIT = False  # if False, the whole data set
-
+MOL_SIZES_OF_INTEREST = [10, 11, 12, 13, 14, 15, 16, 17, 18]  # number of atoms
+SAMPLE_SIZE = 100
 
 def main():
     print('I run main')
@@ -45,6 +50,28 @@ def main():
 
     dict_atoms_num_mol_count = dict(zip(np.arange(len(occurences_number)), occurences_number))
     print(dict_atoms_num_mol_count)
+
+    dict_num_atoms_mol_idx = {}
+    for mol_size_iterate in MOL_SIZES_OF_INTEREST:
+        print(f'mol size of interest = {mol_size_iterate}')
+        dict_num_atoms_mol_idx[mol_size_iterate] = [mol_idx for mol_idx, mol_size in enumerate(atoms_count_list) if mol_size == mol_size_iterate]
+
+    current_path = DATA_FOLDER + '/' + 'dict_num_atoms_mol_idx.pkl'
+    with open(current_path, 'wb') as fid:
+        pickle.dump(dict_num_atoms_mol_idx, fid)
+
+    # dict that contains mol idx of random molecules with X number of atoms. X = 10...18 (why 18 is included, no idea)
+    dict_100 = {}
+    for mol_size_iterate in MOL_SIZES_OF_INTEREST:
+        dict_100[mol_size_iterate] = random.sample(population=dict_num_atoms_mol_idx[mol_size_iterate], k=SAMPLE_SIZE)
+
+    current_path = DATA_FOLDER + '/' + str(SAMPLE_SIZE)
+    if not os.path.exists(current_path):
+        os.mkdir(current_path)
+        for i in MOL_SIZES_OF_INTEREST:
+            file_name = current_path + '/' + str(i)
+            np.savetxt(fname=file_name, X=dict_100[i], fmt=)
+
     print('I am done')
 if __name__ == '__main__':
     main()
