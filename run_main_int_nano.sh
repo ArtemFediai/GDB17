04126e1f51e3c2784a7741473a6b91729bd87ba4
Fediai <bh5670@int-nano.int-nano.int.kit.edu>
2021-11-12 13:02:21 +0100

    sh file for int-nano

---
+++ run_main_int_nano.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=26
+#SBATCH --time=0-03:00:00
+#SBATCH --mem-per-cpu=2000
+#SBATCH --job-name=main
+#SBATCH --partition=batch
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=artem.fediai@kit.edu
+#SBATCH --error out_%j.err
+#SBATCH --output out_%j.out
+
+
+eval "$(conda shell.bash hook)"
+#cd ${SLURM_SUBMIT_DIR}
+conda activate my-rdkit-env
+
+python main.py
